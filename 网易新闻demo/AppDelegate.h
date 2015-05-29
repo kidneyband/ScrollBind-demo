bf519463a1e41101398e1c8b3069379961d420e5
YaJun <yajun.chen@ys-os.com>
2015-05-29 15:00:27 +0800

    Initial Commit

---
+++ 网易新闻demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  网易新闻demo
+//
+//  Created by user on 15-5-29.
+//  Copyright (c) 2015年 user. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
